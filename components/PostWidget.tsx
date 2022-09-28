ea2700440e660ba1113b96d352eec8f525ec6d5d
velopace <velopace@outlook.com>
2022-09-27 20:18:46 -0500

    postwidget with related posts

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -1,10 +1,57 @@
-import * as React from 'react'
+import moment from 'moment';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react'
 
-type Props = {}
+import { getRecentPosts, getSimilarPosts } from '../services'
+import { Post } from '../types';
 
-const PostWidget: React.FC<Props> = () => {
+type Props = {
+    categories?: string[];
+    slug?: string;
+}
+
+const PostWidget: React.FC<Props> = ({ categories, slug }) => {
+    const [relatedPosts, setRelatedPosts] = useState<Post[]>([])
+
+    useEffect(() => {
+        if (slug) {
+            getSimilarPosts(categories, slug)
+                .then((result) => setRelatedPosts(result))
+        } else {
+            getRecentPosts()
+                .then((result) => setRelatedPosts(result))
+        }
+    }, [slug])
+
+    console.log(relatedPosts)
+
     return (
-        <div>PostWidget</div>
+        <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+            <h3 className="text-xl mb-8 font font-semibold border-b pb-4">
+                {slug ? 'Related Posts' : 'Recent Posts'}
+            </h3>
+            {relatedPosts.map((post) => (
+                <div key={post.title} className="flex items-center w-full mb-4">
+                    <div className="w-16 flex-none">
+                        <img
+                            alt={post.title}
+                            height="60px"
+                            width="60px"
+                            className="align-middle rounded-full"
+                            src={post.featuredImage.url}
+                        />
+                    </div>
+                    <div className="flex-grow ml-4">
+                        <p className="text-gray-500 font-xs">
+                            {moment(post.createdAt).format('MMM DD YYYY')}
+                        </p>
+                        <Link href={`/post/${post.slug}`} key={post.title} className="text-md">
+                            {post.title}
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </div>
     );
 }
 
--- services/index.tsx
+++ services/index.tsx
@@ -1,10 +1,12 @@
 import { request, gql } from "graphql-request"
 
+import { Post, PostNode } from "../types";
+
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT || ''
 
-export const getPosts = async () => {
+export const getPosts = async (): Promise<PostNode[]> => {
     const query = gql`
-        query Assets {
+        query GetPosts {
             postsConnection {
                 edges {
                     node {
@@ -36,4 +38,48 @@ export const getPosts = async () => {
     const result = await request(graphqlAPI, query);
 
     return result.postsConnection.edges;
+}
+
+export const getRecentPosts = async (): Promise<Post[]> => {
+    const query = gql`
+        query GetRecentPosts {
+            posts(
+                orderBy: createdAt_ASC
+                last: 3
+            ) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `
+
+    const result = await request(graphqlAPI, query);
+
+    return result.posts;
+}
+
+export const getSimilarPosts = async (categories?: string[], slug?: string): Promise<Post[]> => {
+    const query = gql`
+        query GetSimilarPosts($slug: String!, $categories: [String!]) {
+            posts(
+                where: { slug_not: $slug, AND { categories_some: { slug_in: $categories } } }
+                last: 3
+            ) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `
+
+    const result = await request(graphqlAPI, query, { categories, slug });
+
+    return result.posts;
 }
