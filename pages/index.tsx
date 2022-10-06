567949342cc3b6c80b254348243cfddadd1dc70b
velopace <velopace@outlook.com>
2022-10-05 23:55:52 -0500

    featured posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 
 import { PostCard, Categories, PostWidget } from '../components'
+import { FeaturedPosts } from '../sections'
 import { getPosts } from '../services'
 import { PostNode } from '../types'
 
@@ -16,6 +17,7 @@ const Home: NextPage<Props> = ({ posts }: Props) => {
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => <PostCard post={post.node} key={post.node.slug} />)}
