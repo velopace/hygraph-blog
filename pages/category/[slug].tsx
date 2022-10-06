59f279eee797055621a02c936529dec41071428c
velopace <velopace@outlook.com>
2022-10-06 00:11:41 -0500

    link category to posts

---
+++ pages/category/[slug].tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import { ParsedUrlQuery } from 'querystring'
+
+import { getCategories, getCategoryPost } from '../../services'
+import { PostCard, Categories, Loader } from '../../components'
+import { PostNode } from '../../types'
+import { GetStaticPaths, GetStaticProps } from 'next'
+
+type Props = {
+  posts: PostNode[]
+}
+
+export interface QParams extends ParsedUrlQuery {
+  slug?:string
+}
+
+const CategoryPost = ({ posts }: Props) => {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <Loader />
+  }
+
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative lg:sticky top-8">
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default CategoryPost
+
+// Fetch data at build time
+export const getStaticProps: GetStaticProps<Props, QParams> = async ({ params }) => {
+  const posts = await getCategoryPost(params?.slug || '')
+
+  return {
+    props: { posts },
+  }
+}
+
+// Specify dynamic routes to pre-render pages based on data.
+// The HTML is generated at build time and will be reused on each request.
+export const getStaticPaths: GetStaticPaths = async () => {
+  const categories = await getCategories()
+  return {
+    paths: categories.map(({ slug }) => ({ params: { slug } })),
+    fallback: true,
+  }
+}
